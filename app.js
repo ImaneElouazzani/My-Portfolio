44731528bdb73e93064f2f2af56953d4c657e166
Imane Elouazzani <denjala.smiley@gmail.com>
2021-12-14 17:36:23 +0100

    section footer terminée

--- app.js
+++ app.js
@@ -44,4 +44,15 @@ typewriter
 
 const input_fields = document.querySelectorAll('input')
 
-for(let i = 0; )
+for(let i = 0; i < input_fields.length; i++) {
+
+    let field = input_fields[i]
+
+    field.addEventListener('input', (e) => {
+        if(e.target.value !== '') {
+            e.target.parentNode.classList.add('animation')
+        } else if (e.target.value == '') {
+            e.target.parentNode.classList.remove('animation')
+        }
+    })
+}
